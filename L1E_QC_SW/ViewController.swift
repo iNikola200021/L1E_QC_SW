9420123dff24c7ab0b4021fba8a0d19ad89553fc
iNikola200021 <nikolay200021@gmail.com>
2022-08-25 00:40:40 +0400

    Initial Commit

---
+++ L1E_QC_SW/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  L1E_QC_SW
+//
+//  Created by Николай Колпиков on 25.08.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
